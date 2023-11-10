16a17fee4b7a162a0490545ee975159bf10852ba
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2023-11-10 14:45:43 +0200

    Added linter report
    linter report on csv_parser.py

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -1,6 +1,7 @@
 import csv
 import statistics
 
+
 def parse_csv(file_path):
     if not file_path.endswith('.csv'):
         raise csv.Error("File is not a CSV file")
@@ -8,6 +9,7 @@ def parse_csv(file_path):
         csvreader = csv.DictReader(csvfile)
         data = list(csvreader)
     return data
+
 
 class CSVParser:
     def __init__(self, data):
@@ -21,39 +23,39 @@ class CSVParser:
         for row in self.data:
             try:
                 total += float(row[column_name])
-            except ValueError:
-                raise ValueError("Value is not a number")
+            except ValueError as exc:
+                raise ValueError("Value is not a number") from exc
         return total
 
     def min_max_avg(self, column_name):
         values = []
 
-        assert values == [], "Values should be empty in the beginning"
+        assert not values, "Values should be empty in the beginning"
 
         for row in self.data:
             try:
                 value = float(row[column_name])
                 values.append(value)
-            except ValueError:
-                raise ValueError("Value is not a number")
-        
+            except ValueError as exc:
+                raise ValueError("Value is not a number") from exc
+
         minimum = min(values)
         maximum = max(values)
         average = statistics.mean(values)
 
-        assert maximum != None, "Maximum should not be None"
-        assert minimum != None, "Minimum should not be None"
-        assert average != None, "Average should not be None"
+        assert maximum is not None, "Maximum should not be None"
+        assert minimum is not None, "Minimum should not be None"
+        assert average is not None, "Average should not be None"
 
         return minimum, maximum, average
 
     def shortest_longest_string(self, column_name):
         strings = [row[column_name] for row in self.data if column_name in row]
-        
+
         shortest = min(strings, key=len)
         longest = max(strings, key=len)
 
-        assert shortest != None, "Shortest should not be None"
-        assert longest != None, "Longest should not be None"
+        assert shortest is not None, "Shortest should not be None"
+        assert longest is not None, "Longest should not be None"
 
         return shortest, longest
